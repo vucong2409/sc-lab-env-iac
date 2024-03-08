0f123f4d851c4fce8b0c3b0526f5c1b603af0e44
Cong <cong.vd@teko.vn>
2024-03-08 23:31:47 +0700

    test

--- lab1/main.tf
+++ lab1/main.tf
@@ -2,8 +2,8 @@ terraform {
   // Hard code bucket information instead of using variable since Terraform not support it.
   // See: https://github.com/hashicorp/terraform/issues/13022
   backend "s3" {
-      bucket = "sc-lab-state-bucket"
-    key    = "tf-state/lab-01/dev/state"
+    bucket = "sc-lab-state-bucket"
+      key    = "tf-state/lab-01/dev/state"
     region = "ap-southeast-1"
   }
 }
