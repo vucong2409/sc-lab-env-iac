606c61faa8cda6f86ade04b5641f089fcc0d2aab
Cong <cong.vd@teko.vn>
2024-03-09 11:50:55 +0700

    change region

--- modules/basic-network/version.tf
+++ modules/basic-network/version.tf
@@ -4,7 +4,11 @@ terraform {
         aws = {
             source = "hashicorp/aws"
             version = "~> 5.0"
-            region = "ap-southeast-1"
         }
     }
+}
+
+# Set the region to avoid error/misplace resource.
+provider "aws" {
+  region = "ap-southeast-1"
 }
