93c04f13d11fba2e7087734512b58979d653f1b2
kong <vucong2409@gmail.com>
2024-03-10 16:54:13 +0700

    feat: enable auto provision ip for instance in public subnet

--- modules/basic-network/subnet.tf
+++ modules/basic-network/subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "main_public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = var.public_subnet_cidr
   availability_zone = var.subnet_az
+  map_public_ip_on_launch = "true"
 
   tags = var.general_tags
 }
