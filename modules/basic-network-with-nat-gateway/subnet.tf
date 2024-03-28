2c0446e6b964d22fc8e6760786258536d19a3d06
Inco1122 <vucong2409@gmail.com>
2024-03-28 12:30:02 +0700

    feat: remove peering between 2 VPC (#10)
    * feat: remove peering between 2 VPC and add jumphost

--- modules/basic-network-with-nat-gateway/subnet.tf
+++ modules/basic-network-with-nat-gateway/subnet.tf
@@ -15,6 +15,16 @@ resource "aws_subnet" "main_private_subnet" {
   tags = var.general_tags
 }
 
+resource "aws_route_table_association" "main_public_subnet_rt_assoc" {
+  subnet_id = aws_subnet.main_public_subnet.id
+  route_table_id = aws_route_table.main_public_subnet_rt.id
+}
+
+resource "aws_route_table_association" "main_private_subnet_rt_assoc" {
+  subnet_id = aws_subnet.main_private_subnet.id
+  route_table_id = aws_route_table.main_private_subnet_rt.id
+}
+
 resource "aws_network_acl_association" "main_public_subnet_nacl_assoc" {
   network_acl_id = aws_network_acl.nacl_allow_all.id
   subnet_id      = aws_subnet.main_public_subnet.id
