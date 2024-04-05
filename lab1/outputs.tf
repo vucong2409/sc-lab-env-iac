8ecaa304c1f73f12bae6fa975e4504cc0615882d
kong <vucong2409@gmail.com>
2024-04-05 14:19:36 +0700

    feat: change server_admins to posixgroup & add it to sudoers

--- lab1/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "endpoint_ldap_server" {
-  value       = local.endpoint_ldap_server
-  description = <<EOT
-  DNS Endpoint of LDAP Server.
-  Please notice that this address only accessible from application VPC.
-  EOT
-}
